a3c2718bf65549c78806fc7c0c74884c3fb406c8
caiomzrocha <caiomzrocha@gmail.com>
2020-03-28 19:27:13 -0300

    Adicionados alguns testes

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,13 @@
 const express = require('express'); //importar o modulo express para a variavel express
 const routes = require('./routes');
 const cors = require('cors');
+const {errors} = require('celebrate');
 
 const app = express(); //variavel app para armazenar a aplicacao
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use(errors());
 
-app.listen(3333); //aplicacao escutando na porta 3333
+module.exports = app;
